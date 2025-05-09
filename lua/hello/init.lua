b66ad4bc1fa1d075260475ccf2ae383aa26f9d9b
dssafford <dssafford@gmail.com>
2025-05-09 10:26:39 -0400

    update

--- lua/hello/init.lua
+++ lua/hello/init.lua
@@ -5,7 +5,7 @@ local M = {}
 function M.setup()
   vim.api.nvim_create_user_command("HelloWorld", function()
     -- vim.api.nvim_echo({ { "Absolutely final Hello from plugin! 2025-05-09 @ 09:56", "None" } }, false, {})
-    vim.api.nvim_echo({ { "🔁 Reloaded @ " .. os.date() } }, false, {})
+    vim.api.nvim_echo({ { "🔁 Hey dude - Reloaded @ " .. os.date() } }, false, {})
   end, {})
 
   vim.keymap.set("n", "<leader>hw", function()
